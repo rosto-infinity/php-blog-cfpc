db08ee8c7a1fb09d6c89c6c53102aaa8ebd13de4
rosto-infinity <rostoinfocus@gmail.com>
2026-05-12 08:30:45 +0100

    feat: Add article display page with comments and sidebar stats

--- resources/views/layouts/admin-layout/admin-sidebar_html.php
+++ resources/views/layouts/admin-layout/admin-sidebar_html.php
@@ -1,21 +1,23 @@
 	<section id="sidebar">
 		<a href="#" class="brand"><i class='bx bxs-smile icon'></i> AdminSite</a>
 		<ul class="side-menu">
-			<li><a href="admin.php" class="active"><i class='bx bxs-dashboard icon' ></i> Dashboard</a></li>
+			<li><a href="admin.php" class="active"><i class='bx bxs-dashboard icon'></i> Dashboard</a></li>
 			<li class="divider" data-text="Gestion Blog">Gestion Blog</li>
+			<li><a href="/"><i class='bx bxs-widget icon'></i>  Accueil Blog</a></li>
+
 			<li>
-				<a href="#"><i class='bx bxs-news icon' ></i> Articles <i class='bx bx-chevron-right icon-right' ></i></a>
+				<a href="#"><i class='bx bxs-news icon'></i> Articles <i class='bx bx-chevron-right icon-right'></i></a>
 				<ul class="side-dropdown">
 					<li><a href="admin-list-article.php">Liste des articles</a></li>
 					<li><a href="admin-add-article.php">Ajouter un article</a></li>
 				</ul>
 			</li>
-		
-			<li><a href="#"><i class='bx bxs-chart icon' ></i> Charts</a></li>
-			<li><a href="#"><i class='bx bxs-widget icon' ></i> Widgets</a></li>
+
+			<li><a href="#"><i class='bx bxs-chart icon'></i> Charts</a></li>
+			<li><a href="#"><i class='bx bxs-widget icon'></i> Widgets</a></li>
 			<li class="divider" data-text="table and forms">Table and forms</li>
-			<li><a href="#"><i class='bx bx-table icon' ></i> Tables</a></li>
-		
+			<li><a href="#"><i class='bx bx-table icon'></i> Tables</a></li>
+
 		</ul>
-		
+
 	</section>
