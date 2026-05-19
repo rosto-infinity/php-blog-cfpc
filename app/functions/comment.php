1beb42db7ff62e420501ab7651ea7d3cf3ebc6c0
rosto-infinity <rostoinfocus@gmail.com>
2026-05-19 09:22:29 +0100

    design patterns: add findCommentsByArticles() to comment.php and use it in user-article-show.php

--- app/functions/comment.php
+++ app/functions/comment.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 require_once __DIR__ . '/../../database/database.php';
@@ -8,7 +9,22 @@ require_once __DIR__ . '/../../database/database.php';
  */
 function countComments(): int
 {
-    $pdo = getPdo();
-    $query = $pdo->query('SELECT COUNT(*) FROM comments');
-    return (int) $query->fetchColumn();
+  $pdo = getPdo();
+  $query = $pdo->query('SELECT COUNT(*) FROM comments');
+  return (int) $query->fetchColumn();
+}
+
+function findCommentsByArticles(int $article_id): array
+{
+  $pdo = getPdo();
+  $sql = 'SELECT comments.*, users.username
+          FROM comments
+          JOIN users
+          ON comments.user_id = users.id
+          WHERE article_id= :article_id
+          ORDER BY comments.created_at DESC';
+
+  $query = $pdo->prepare($sql);
+  $query->execute(compact('article_id'));
+  return  $query->fetchAll();
 }
--- user-article-show.php
+++ user-article-show.php
@@ -11,16 +11,7 @@ require_once 'app/helpers.php';
 $article_id = $_GET['id'];
 $article =findArticle((int)$article_id);
 
-$sql = 'SELECT comments.*, users.username
- FROM comments
- JOIN users ON comments.user_id = users.id
- WHERE article_id= :article_id';
- 
-$query = $pdo->prepare($sql);
-$query->execute(compact('article_id'));
-$commentaires = $query->fetchAll();
-
-
+$commentaires = findCommentsByArticles((int) $article_id);
 
 //Statistiques
   $usersCount = countUsers();
